02eabdab62cf9ad1af5f097c513450f48e3ba427
dorian hardy <thegostisdead@gmail.com>
2020-01-14 10:18:43 +0100

    pretify code

--- versionClassique/plateau.py
+++ versionClassique/plateau.py
@@ -26,20 +26,23 @@ def Plateau(nbJoueurs, nbTresors): #modifier la génération de joueur
     plateau = Matrice(7,7) # on créer le plateau 
     ligne_plateau = getNbLignes(plateau) # récupére le nombre de ligne de plateau
     colonne_plateau = getNbColonnes(plateau) # récupére le nombre de colonne de plateau
-    liste_trésor = []
-    liste_joueur = []
+    liste_tresor = []
+
     
     for x in range(ligne_plateau):
         for y in range(colonne_plateau):
-            trésor = random.randint(1,nbTresors) # génére de maniére aléatoire un numéro de trésor en tre 1 et nbTresor
+            tresor = random.randint(1,nbTresors) # génére de maniére aléatoire un numéro de trésor en tre 1 et nbTresor
             #joueur = random.randint(1,4) # génére de maniére aléatoire un numéro de pion
             #génére une carte aléatoire avec un nombre aléatoire aléatoire d'un trésor (1 à nbTresor)
             #génére un joueur aléatoire
-            if trésor not in liste_trésor or liste_trésor == []:
-                info_carte = Carte(bool(random.randint(0,1)),bool(random.randint(0,1)),bool(random.randint(0,1)),bool(random.randint(0,1)),trésor)
+            if tresor not in liste_tresor or liste_tresor == []:
+                info_carte = Carte(bool(random.randint(0,1)),\
+                                   bool(random.randint(0,1)),\
+                                   bool(random.randint(0,1)),\
+                                   bool(random.randint(0,1)),\
+                                   tresor)
             setVal(plateau,x,y,info_carte)
-            liste_trésor.append(trésor)
-            #liste_joueur.append(joueur)
+            liste_tresor.append(tresor)
     print(plateau)  
 Plateau(4,46) #test
 
